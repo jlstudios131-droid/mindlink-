a3ccd7b7518778358ba04988254a36b3954a04ce
JLStudioOficiall <jlstudios131@gmail.com>
2025-10-18 23:09:45 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// Vite configuration for React + Tailwind project
+export default defineConfig({
+  plugins: [react()],
+});
